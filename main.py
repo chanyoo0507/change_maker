a0d74aac8ee23516d0b6a2f7156f145ce6ae8ac0
chanyoo0507 <chanyoo0507@gmail.com>
2026-06-20 13:03:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def load_file(uploaded_file):
     return df
 
 def make_question(df):
-    q_index = random.randint(0,len(df["question_index"]))
+    q_index = random.randint(0,len(df["question_id"]))
     question = df["질문"][q_index]
     answer_list = df["답변"]
     answer = answer_list[q_index]
