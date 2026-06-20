54eb94d007a1031fdd7c584114ee44cf602453cd
chanyoo0507 <chanyoo0507@gmail.com>
2026-06-20 14:02:53 +0900

    Update main.py

--- main.py
+++ main.py
@@ -36,6 +36,8 @@ def make_question(df):
             wrong_answers.remove(wrong_answer)
     return {'question':question,'answers':answers,'answer':answers[a_index]}
 
+st.session_state.setdefault("next", 0)
+
 animations = load_animation()
 uploaded_file = st.file_uploader("엑셀 파일을 업로드하세요",type=["xlsx"])
 if uploaded_file is not None:
@@ -43,6 +45,8 @@ if uploaded_file is not None:
     next = st.button("다음으로")
     if next:
         question = make_question(df)
+        st.session_state["next"] = 1
+    if st.session_state["next"] == 1:
         answer = st.radio(question['question'],question['answers'],index=None)
         if answer is not None:
             if answer == question['answer']:
