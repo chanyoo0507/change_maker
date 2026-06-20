2e48788b6fe42d30ae8530df68f07feeff0da164
chanyoo0507 <chanyoo0507@gmail.com>
2026-06-20 19:18:12 +0900

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def select_question(df, mode):
     if mode == "랜덤":
         question_id = random.randint(0,len(df["question_id"])-1)
     elif mode == "순서 섞기":
-        if st.session_state["questions_left"] == []:
+        if len(st.session_state["questions_left"])==0:
             st.session_state["questions_left"] == df["question_id"].tolist()
         question_id = (st.session_state["questions_left"])[random.randint(0,len(st.session_state["questions_left"])-1)]
         st.session_state["questions_left"].remove(question_id)
